c30d8570ca812e9a86e0789e890dd7682f130bf9
Sumuditha-Janith <sumuditha356@gmail.com>
2025-02-11 16:17:17 +0530

    order and order details

---
+++ src/main/java/lk/ijse/gdse/loslibros/bo/custom/PalaceOrderBO.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse.loslibros.bo.custom;
+
+public interface PalaceOrderBO {
+}
---
+++ src/main/java/lk/ijse/gdse/loslibros/bo/custom/impl/PalaceOrderBOImpl.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse.loslibros.bo.custom.impl;
+
+public class PalaceOrderBOImpl {
+}
